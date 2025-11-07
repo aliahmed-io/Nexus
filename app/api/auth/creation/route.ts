7494b0e5af426969f327e9aef334b0baf938164a
alinewprogrammer <alihassan182006@gmail.com>
2025-11-07 13:43:09 +0300

    Update production redirect URL in auth creation route

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://nexus-zeta-two-88.vercel.app/"
+      : "https://nexus-xi-eight.vercel.app/"
   );
 }
